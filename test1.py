67996cfaedbfcb08f8213f237cbca94904110982
StormInside <storminside4766@gmail.com>
2021-08-23 23:04:15 +0300

    Problems with setFocus

--- test1.py
+++ test1.py
@@ -1,16 +1,24 @@
 import sys
-from PyQt5.QtCore import Qt, QPoint
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QSystemTrayIcon, QStyle, QAction, qApp, QMenu
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, \
+                            QMainWindow, \
+                            QSystemTrayIcon, \
+                            QStyle, \
+                            QAction, \
+                            qApp, \
+                            QMenu, \
+                            QShortcut
 
-class cssden(QMainWindow):
-    def __init__(self):
+
+class Main(QMainWindow):
+    def __init__(self, width=1800, height=600, x=60, y=0):
         super().__init__()
 
-        # <MainWindow Properties>
-        self.setFixedSize(1280, 720)
+        self.setFixedSize(width, height)
+        self.move(x, y)
         self.setStyleSheet("QMainWindow{background-color: darkgray;border: 1px solid black}")
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool)
-        # </MainWindow Properties>
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool)  # TODO Create icon
 
         self.tray_icon = QSystemTrayIcon(self)
         self.tray_icon.setIcon(self.style().standardIcon(QStyle.SP_ComputerIcon))
@@ -22,27 +30,36 @@ class cssden(QMainWindow):
         show_action.triggered.connect(self.show)
         hide_action.triggered.connect(self.hide)
         quit_action.triggered.connect(qApp.quit)
+
         tray_menu = QMenu()
         tray_menu.addAction(show_action)
         tray_menu.addAction(hide_action)
         tray_menu.addAction(quit_action)
+
         self.tray_icon.setContextMenu(tray_menu)
         self.tray_icon.show()
 
-        self.oldPos = self.pos()
+        self.show_hide_key = QShortcut(QKeySequence('Ctrl+F'), self)
+        self.show_hide_key.activated.connect(self.show_hide)
+
         self.show()
 
+        self.setFocus()
+        app.focusChanged.connect(self.on_focus_change)
 
-    def mousePressEvent(self, event):
-        self.oldPos = event.globalPos()
+    def on_focus_change(self):
+        print(self.hasFocus())
+        if not self.isActiveWindow():
+            self.hide()
 
-    def mouseMoveEvent(self, event):
-        delta = QPoint (event.globalPos() - self.oldPos)
-        self.move(self.x() + delta.x(), self.y() + delta.y())
-        self.oldPos = event.globalPos()
+    def show_hide(self):
+        if self.isVisible():
+            self.hide()
+        else:
+            self.show()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = cssden()
+    main = Main()
     sys.exit(app.exec_())
