521a96f5899ec67eeb3b825071870d0f5b2688b6
utilka <andriy.v.bolshakov@gmail.com>
2021-08-30 20:57:25 +0300

    Read_Changed_Key_Seq

--- Settings.py
+++ Settings.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QKeySequence
 from PyQt5.QtWidgets import QApplication, \
                             QMainWindow, \
                             QSystemTrayIcon, \
@@ -28,7 +28,6 @@ def start_settings_constr(app, size, window_to_hide):
 class Settings(QMainWindow, settigsUi.Ui_Settings):
     def __init__(self, width=1280, height=720, x=0.5, y=0):
         super().__init__()
-
         self.setupUi(self)
 
         self.setFixedSize(width, height)
@@ -39,7 +38,17 @@ class Settings(QMainWindow, settigsUi.Ui_Settings):
 
         self.setWindowTitle("Settings")
         self.setStyleSheet("QMainWindow{background-color: black;border: 1px solid white}")
+
+        self.hotkey_input.keySequenceChanged.connect(self._change_key_sequence)
         # self.setWindowFlags()
+
+    def _change_key_sequence(self,new_key_seq:QKeySequence):
+        print (new_key_seq.toString())
+        # basic : take first key from key combination
+        # detail: convert new_key_seq to string, split string into list of key combinations, take first key combination, strip it from spaces, create QKeySequence from it
+        mod_key_seq = QKeySequence((new_key_seq.toString()).split(',')[0].strip())
+        self.hotkey_input.setKeySequence(mod_key_seq)
+        # update_settings() #TODO
 
     # def closeEvent(self, event):
     # TODO disable hotkeys when in settings
